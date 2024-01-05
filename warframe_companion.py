6d6f1c25c5c981a3f40394b7d207fd997c0b10eb
tyler <tylerjonahmccoy@gmail.com>
2024-01-05 02:15:11 -0600

    Added basic channel functionality

--- warframe_companion.py
+++ warframe_companion.py
@@ -4,19 +4,25 @@ import dotenv
 import os
 
 dotenv.load_dotenv()
-
-print(wf_api.get_platforms()) # It's always possible to retrieve all supported platforms with which to construct the API, even in a static context.
-pc = wf_api("pc")
-current_fissures = pc.get_fissure_info()
-
-fissure_objects = [Fissure(fissure) for fissure in current_fissures]
-
-for f in fissure_objects:
-    print(type(f), f.enemy, f.missionType)
+category_name = "Warframe Companion Bot"
+channel_names = ["arbitration", "archon-hunt", "void-trader", "world-timers", "fissures", "sortie", "steel-path"]
 
 class MyClient(d.Client):
     async def on_ready(self):
         print(f"Logged on as {self.user}!")
+        for guild in self.guilds:
+            category = None
+            for cat in guild.categories: 
+                 if cat.name == category_name:
+                     category = cat
+
+            if category is None:
+                category = await guild.create_category(category_name)
+
+            for channel in channel_names:
+                if channel not in [channel.name for channel in category.channels]:
+                    await category.create_text_channel(channel)
+    
     async def on_message(self, message):
         print(f"Message from {message.author}: {message.content}")
     
