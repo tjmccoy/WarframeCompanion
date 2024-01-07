0d48056f4ebda3d23fee4e8e6e20445dbe3a3c03
tyler <tylerjonahmccoy@gmail.com>
2024-01-06 18:40:07 -0600

    Switched to bot and implemented slash commands.

--- warframe_companion.py
+++ warframe_companion.py
@@ -6,25 +6,30 @@ import os
 dotenv.load_dotenv()
 category_name = "Warframe Companion Bot"
 channel_names = ["arbitration", "archon-hunt", "void-trader", "world-timers", "fissures", "sortie", "steel-path"]
+bot = d.Bot(intents = d.Intents.default())
 
-class MyClient(d.Client):
-    async def on_ready(self):
-        print(f"Logged on as {self.user}!")
-        for guild in self.guilds:
-            category = None
-            for cat in guild.categories: 
-                 if cat.name == category_name:
-                     category = cat
+@bot.event
+async def on_ready():
+    print(f"Logged on as {bot.user}!")
+    for guild in bot.guilds:
+        await make_channels(guild)
 
-            if category is None:
-                category = await guild.create_category(category_name)
+async def make_channels(guild):
+    category = None
+    for cat in guild.categories: 
+        if cat.name == category_name:
+            category = cat
 
-            for channel in channel_names:
-                if channel not in [channel.name for channel in category.channels]:
-                    await category.create_text_channel(channel)
+    if category is None:
+        category = await guild.create_category(category_name)
+
+    for channel in channel_names:
+        if channel not in [channel.name for channel in category.channels]:
+            await category.create_text_channel(channel)
     
-    async def on_message(self, message):
-        print(f"Message from {message.author}: {message.content}")
-    
-client = MyClient(intents = d.Intents.all())
-client.run(os.getenv("TOKEN"))
+@bot.slash_command(description = "Creates WC text channels for your server.", name = "make-channels")
+async def slash_make_channels(ctx):
+    await ctx.respond("Creating channels...")
+    await make_channels(ctx.guild)
+        
+bot.run(os.getenv("TOKEN"))
