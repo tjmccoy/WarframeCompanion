fe5c452bc04c07fe76233a622bfbbc0a58f0d8ae
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2024-01-25 21:10:58 -0800

    Implemented void trader (#6)
    * Implemented void trader
    
    * Cleaned up newlines

--- warframe_companion_core.py
+++ warframe_companion_core.py
@@ -1,7 +1,7 @@
 import discord as d
 from discord_helpers import *
 from enum import Enum
-import pylotus as pl
+import pyframe
 
 CATEGORY_NAME = "Warframe Companion Bot"
 
@@ -28,14 +28,30 @@ async def make_channels(guild: d.Guild):
     for channel_name in ChannelName:
         await make_channel(guild, channel_name.value, CATEGORY_NAME)
 
-async def display_fissures(guild: d.Guild, current_fissures: dict):
+async def display_fissures(guild: d.Guild, current_fissures: list[pyframe.Fissure]):
     await wc_clear_channel(guild, ChannelName.fissures)
     information = ""
-    fissure_objects = [pl.Fissure(fissure) for fissure in current_fissures]
-    for f in fissure_objects:
-        information += f"{f.node} | {f.missionType} ({f.tier})" + "\n"
+    for f in current_fissures:
+        information += f"{f.node} | {f.mission_type} ({f.tier})" + "\n"
     await wc_send_to_channel(guild, information, ChannelName.fissures)
 
 async def display_arbitration(guild: d.Guild):
     await wc_clear_channel(guild, ChannelName.arbitration)
     await wc_send_to_channel(guild, "Arbitration information currently unavailable", ChannelName.arbitration)
+
+async def display_void_trader(guild: d.Guild, void_trader: pyframe.VoidTrader):
+    await wc_clear_channel(guild, ChannelName.void_trader)
+    information = ""
+    void_trader = pyframe.get_void_trader()
+    if not void_trader.active:
+        await wc_send_to_channel(guild, f"Void trader is not here. He will return on {str(void_trader.activation).split('+')[0]} UTC.", ChannelName.void_trader)
+        return
+    
+    information += f"Location: {void_trader.location}\n"
+    information += f"Next Arrival: {void_trader.activation}\n"
+    information += f"Next Departure: {void_trader.expiry}\n"
+    information += f"Inventory:\n"
+    for i in void_trader.inventory:
+        information += f"{i.item} | {i.ducats} ducats | {i.credits} credits\n"
+
+    await wc_send_to_channel(guild, information, ChannelName.void_trader)
